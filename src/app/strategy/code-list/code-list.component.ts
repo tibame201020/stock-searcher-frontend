7351c98b896f1e0820867580870e445406b40841
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-07-03 00:33:08 +0000

    add sticky

--- src/app/strategy/code-list/code-list.component.ts
+++ src/app/strategy/code-list/code-list.component.ts
@@ -9,47 +9,44 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-code-list',
   templateUrl: './code-list.component.html',
-  styleUrls: ['./code-list.component.css']
+  styleUrls: ['./code-list.component.css'],
 })
 export class CodeListComponent implements OnInit {
+  keyword: string = '';
+  user: string = 'dev-user';
+  codeListArray: CodeList[] = [];
 
-  keyword:string='';
-  user:string = 'dev-user';
-  codeListArray:CodeList[] = [];
-
-  constructor(private stockService:StockService, public dialog: MatDialog){
-
-  }
+  constructor(private stockService: StockService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
-    this.getCodeListByUser(this.user)
+    this.getCodeListByUser(this.user);
   }
 
-  openCodeListResult(){
+  openCodeListResult() {
     let dialogRef = this.dialog.open(CodeListResultComponent, {
       width: '90%',
-      height:'60%',
+      height: '60%',
       data: {
-        user:this.user
+        user: this.user,
       },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      this.getCodeListByUser(this.user)
+    dialogRef.afterClosed().subscribe((result) => {
+      this.getCodeListByUser(this.user);
     });
   }
 
-  createCodeList(codeListId?:string) {
+  createCodeList(codeListId?: string) {
     let dialogRef = this.dialog.open(CodeListEditComponent, {
       width: '80%',
-      height:'60%',
+      height: '60%',
       data: {
-        codeListId:codeListId
+        codeListId: codeListId,
       },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      this.getCodeListByUser(this.user)
+    dialogRef.afterClosed().subscribe((result) => {
+      this.getCodeListByUser(this.user);
     });
   }
 
@@ -59,52 +56,48 @@ export class CodeListComponent implements OnInit {
     });
   }
 
-  editOrDelCodeList(codeList:CodeList, idx:number) {
-    console.log(codeList)
+  editOrDelCodeList(codeList: CodeList, idx: number) {
     Swal.fire({
-      toast:true,
+      toast: true,
       title: 'CodeList: ' + codeList.name + '?',
       showCancelButton: true,
-      cancelButtonText:'取消',
+      cancelButtonText: '取消',
       confirmButtonText: '編輯',
-      showDenyButton:true,
-      denyButtonText:'刪除',
+      showDenyButton: true,
+      denyButtonText: '刪除',
     }).then((result) => {
       if (result.isConfirmed) {
-        this.createCodeList(codeList.codeListId)
+        this.createCodeList(codeList.codeListId);
       }
       if (result.isDenied) {
         this.removeCodeList(codeList, idx);
       }
-    })
+    });
   }
 
-  removeCodeList(codeList:CodeList, idx:number) {
+  removeCodeList(codeList: CodeList, idx: number) {
     Swal.fire({
-      toast:true,
-      title: `確定移除codeList: `+ codeList.name + `?`,
+      toast: true,
+      title: `確定移除codeList: ` + codeList.name + `?`,
       showCancelButton: true,
-      cancelButtonText:'Cancel',
+      cancelButtonText: 'Cancel',
       confirmButtonText: 'Confirm',
     }).then((result) => {
       if (result.isConfirmed) {
-        this.stockService.deleteCodeList(codeList.codeListId).subscribe(
-          res => {
-            this.codeListArray.splice(idx, 1)
+        this.stockService
+          .deleteCodeList(codeList.codeListId)
+          .subscribe((res) => {
+            this.codeListArray.splice(idx, 1);
             Swal.fire({
               icon: 'success',
               title: 'delete successful',
-              position:'center',
-              showConfirmButton:false,
-              timer:500,
-              toast: true
-            }).then(result => {
-
-            })
-          }
-        )
+              position: 'center',
+              showConfirmButton: false,
+              timer: 500,
+              toast: true,
+            }).then((result) => {});
+          });
       }
-    })
+    });
   }
-
 }
